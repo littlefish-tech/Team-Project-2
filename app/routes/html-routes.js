01633314e6a27b8f02bc99bc60920f6d9bf62260
Andrew Lu <awclu@ucdavis.edu>
2020-01-22 18:36:01 -0800

    finalize server side things

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -14,17 +14,21 @@ module.exports = function(app) {
 
   // index route loads view.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
   // Route to the cms page
-  app.get("/cms", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/cms.html"));
+  app.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/loginPage.html"));
   });
 
   // blog route loads blog.html
-  app.get("/blog", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+  app.get("/register", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/registerPage.html"));
+  });
+
+  app.get("/user", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/userAccount.html"));
   });
 
 };
