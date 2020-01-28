89ca2353dd291141f19954c7632448f59c4646e1
Andrew Lu <awclu@ucdavis.edu>
2020-01-27 17:56:28 -0800

    added prelim rating

--- app/public/js/search.js
+++ app/public/js/search.js
@@ -100,10 +100,19 @@ $(".srcBut").on("click", function(event) {
     // -----------------------------------------------------------------------
 });
 
+
 $(document).on("click", "#v-pills-saved-tab", function(){
-  
+  $("#srcRes1").empty();
   savedMedia();
 });
+
+$(document).on("click", ".far", function(event){
+  event.preventDefault();
+  var movieName = $(this).parent()[0].classList[1];
+  var movieRating = $(this)[0].id;
+
+  console.log("You gave "+ movieName + " " + movieRating + " Stars!");
+})
 
 function savedMedia(){
   
@@ -122,15 +131,24 @@ function savedMedia(){
     var format = date.split("000Z").join('');
     //Don't judge I know it's crude
     
+    var temp = response[i].media_name;
+    var iden = temp.split(" ").join("");
+
+    
     
     $("#srcRes1").append("<div class='rmv card movieCards1 col-lg-3 col-md-3 col-sm-12'>")
     
+    
     $(".movieCards1").append("<img class = 'card-img-top' src = " + response[i].imageurl + " alt = 'card img'>")
-                    .append("<div class = 'rmv1 card-body'>");
+                    .append("<div class = 'rmv1 card-body'>")
+                    
     $(".card-body").append("<p class = 'cardText title'> " + response[i].media_name)
+                    .append("<i id = '1' class='far fa-star'></i><i id = '2' class='far fa-star'></i><i id = '3' class='far fa-star'></i><i id = '4' class='far fa-star'></i><i id = '5' class='far fa-star'></i>")
+
                     .append("<p class = 'cardText'> Added: " + format)
                     .append("<p class = 'cardText'> " + response[i].release_yr)
-                    .append("<p class = 'cardText'> " + response[i].description);
+                    .append("<p class = 'cardText'> " + response[i].description)
+                   .addClass(iden);
 
     
     $(".rmv").removeClass("movieCards1");
